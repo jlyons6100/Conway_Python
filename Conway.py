050539ce64d127d22f9f93560a51b0c5fe1a9248
James Lyons <james@MacBook-Air-369.local>
2019-08-31 15:34:13 -0400

    Conway Refactoring

--- Conway.py
+++ Conway.py
@@ -29,123 +29,18 @@ class Game():
 				val = 0
 				if rand >= self.chance_zero: val = 1
 				self.board[row].append(val)
-
-
-	def live_helper(self, row, col, visited, i, j, num):
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		return num
-	# Live neighbors
+				
 	def get_live(self, row, col, visited):
 		num = 0
-		i = -1
-		j = -1
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		i = -1
-		j = 0
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		i = -1
-		j = 1
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		i = 0
-		j = -1
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		i = 1
-		j = -1
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		i = 0
-		j = 1
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		i = 1
-		j = 0
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		i = 1
-		j = 1
-		#num = self.live_helper(row, col, visited, i, j, num)
-		r = row + i
-		c = col + j
-		if r < 0: r += len(self.board)
-		elif r > len(self.board) - 1: r -= len(self.board)
-		if c < 0: c += len(self.board)
-		elif c > len(self.board) - 1: c -= len(self.board)
-		val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		if val == 1: num += 1
-		
-		# num2 = 0
-		# for i in range(-1, 2):
-		# 	for j in range(-1, 2):
-		# 		if not (i == 0 and j == 0):
-		# 			r = row + i
-		# 			c = col + j
-		# 			if r < 0: r += len(self.board)
-		# 			elif r > len(self.board) - 1: r -= len(self.board)
-		# 			if c < 0: c += len(self.board)
-		# 			elif c > len(self.board) - 1: c -= len(self.board)
-		# 			val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
-		# 			if val == 1: num2 += 1
-		# if num2 != num: print("MISTAKE")
+		for (i, j) in ((-1,-1),(-1,0),(-1,1),(0,-1),(1,-1),(0,1),(1,0),(1,1)):
+			r = row + i
+			c = col + j
+			if r < 0: r += len(self.board)
+			elif r > len(self.board) - 1: r -= len(self.board)
+			if c < 0: c += len(self.board)
+			elif c > len(self.board) - 1: c -= len(self.board)
+			val = self.board[r][c] if not (r,c) in visited else visited[(r,c)]
+			if val == 1: num += 1
 		return num
 
 	def next_gen(self):
@@ -154,13 +49,15 @@ class Game():
 		for row in range(len(self.board)):
 			for col in range(len(self.board[0])):
 				n  = self.get_live(row,col,visited)
-				#print(n, end=" ")
 				visited[(row, col)] = self.board[row][col]
 				if self.board[row][col] == 1:
 					if n < 2 or n > 3: self.board[row][col] = 0
 				else: 
 					if n == 3: self.board[row][col] = 1
-# c = Game(800, .75)
+# c = Game(50, .75)
 # c.create_board()
-# c.next_gen()
-# cProfile.run('c.next_gen()')
+# for _ in range(50):
+# 	c.next_gen()
+# 	c.print_board()
+# # c.next_gen()
+# cProfile.run('c.next_gen()',sort=1)
